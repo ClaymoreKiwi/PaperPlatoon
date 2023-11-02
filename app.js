1d0f233544d83831c76cea452199d0ed8c36b930
ClaymoreKiwi <109832174+ClaymoreKiwi@users.noreply.github.com>
2023-11-02 16:51:23 +0000

    update server

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-const http = require('http');
-const fs = require('fs');
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-// Create a server object:
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' }); 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-  fs.readFile('Public/MyWebGame/index.html', function (error, data) {
-    if (error) {
-      res.writeHead(404);
-      res.write('File Not Found');
-    } else {
-      res.write(data);
-    }
-    res.end();
-  });
-});
-
-// Set up our server so it will listen on the port
-server.listen(port, function (error) {
-  // Checking if any error occurred while listening on the port
-  if (error) {
-    console.log('Something went wrong', error);
-  } else {
-    console.log('Server is listening on port ' + port); // Added a space before the port
-  }
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
